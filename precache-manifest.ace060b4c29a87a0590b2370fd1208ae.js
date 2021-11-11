7ec9c41f8c33c6a57f64b1ae2f48303c4cafaf2c
paolaPosada <paola.posada1@udea.edu.co>
2021-11-10 19:13:11 -0500

    Updates

---
+++ precache-manifest.ace060b4c29a87a0590b2370fd1208ae.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6778df136aeba86bf8d2d0513db02004",
+    "url": "/Hero_App/index.html"
+  },
+  {
+    "revision": "c55f66aa3c6bd052e99c",
+    "url": "/Hero_App/static/js/2.ef66c46f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Hero_App/static/js/2.ef66c46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a171396dac47c9a07db",
+    "url": "/Hero_App/static/js/main.fc07c746.chunk.js"
+  },
+  {
+    "revision": "38153667a3ece77c5d47",
+    "url": "/Hero_App/static/js/runtime-main.83277c86.js"
+  },
+  {
+    "revision": "a179d8711d59015e5d3d53637ad9fce7",
+    "url": "/Hero_App/static/media/dc-arrow.a179d871.jpg"
+  },
+  {
+    "revision": "8efedfa7bf353070fb1dc56bc36a8bd3",
+    "url": "/Hero_App/static/media/dc-batman.8efedfa7.jpg"
+  },
+  {
+    "revision": "5d203be297ee7aee6df388f48effc2d2",
+    "url": "/Hero_App/static/media/dc-black.5d203be2.jpg"
+  },
+  {
+    "revision": "871a00dda57a453a698369e1687db44e",
+    "url": "/Hero_App/static/media/dc-blue.871a00dd.jpg"
+  },
+  {
+    "revision": "d22cd5d0cdbb8e6cb64ea844fd1082fa",
+    "url": "/Hero_App/static/media/dc-flash.d22cd5d0.jpg"
+  },
+  {
+    "revision": "dae0db0a19c068035a119a4cf07bfd39",
+    "url": "/Hero_App/static/media/dc-green.dae0db0a.jpg"
+  },
+  {
+    "revision": "9629381c9ae8fe6a5c9ab23308cd76f0",
+    "url": "/Hero_App/static/media/dc-martian.9629381c.jpg"
+  },
+  {
+    "revision": "3b3b7dd1a4716a9f8eec3c13c0611726",
+    "url": "/Hero_App/static/media/dc-robin.3b3b7dd1.jpg"
+  },
+  {
+    "revision": "ad88dd859f1b6cb05d235edfdf7d2da9",
+    "url": "/Hero_App/static/media/dc-superman.ad88dd85.jpg"
+  },
+  {
+    "revision": "b01172005be3eba0ddbe50bd07d3ece8",
+    "url": "/Hero_App/static/media/dc-wonder.b0117200.jpg"
+  },
+  {
+    "revision": "71c5a519e43cab5350b5a5dbb45f4ece",
+    "url": "/Hero_App/static/media/marvel-captain.71c5a519.jpg"
+  },
+  {
+    "revision": "017af60c9d646181629f0887c02e8ef0",
+    "url": "/Hero_App/static/media/marvel-cyclops.017af60c.jpg"
+  },
+  {
+    "revision": "3d4c3df826dce8f0a85e8e49bd3fb204",
+    "url": "/Hero_App/static/media/marvel-daredevil.3d4c3df8.jpg"
+  },
+  {
+    "revision": "df41e28ce57042715584e972ea53e5a2",
+    "url": "/Hero_App/static/media/marvel-hawkeye.df41e28c.jpg"
+  },
+  {
+    "revision": "8f5cd9397468b7b508ffa33b00c5014e",
+    "url": "/Hero_App/static/media/marvel-hulk.8f5cd939.jpg"
+  },
+  {
+    "revision": "8f307eef75cd45b3e7076ece1b368e5f",
+    "url": "/Hero_App/static/media/marvel-iron.8f307eef.jpg"
+  },
+  {
+    "revision": "c055e4a00bff140e8faf40eee0701eaa",
+    "url": "/Hero_App/static/media/marvel-silver.c055e4a0.jpg"
+  },
+  {
+    "revision": "9967f816d7adb4c674f99d230e9472c7",
+    "url": "/Hero_App/static/media/marvel-spider.9967f816.jpg"
+  },
+  {
+    "revision": "9f1b74e51e318f14c533fdb3b03a7421",
+    "url": "/Hero_App/static/media/marvel-thor.9f1b74e5.jpg"
+  },
+  {
+    "revision": "a1a41d9adf2f16f1220723a70ce20215",
+    "url": "/Hero_App/static/media/marvel-wolverine.a1a41d9a.jpg"
+  }
+]);
